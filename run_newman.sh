b58f3d912c05ee14298291540fc3db93a429a069
Unang <unang@softwareseni.com>
2025-05-20 14:48:44 +0700

    modify yml file variable

--- run_newman.sh
+++ run_newman.sh
@@ -1,5 +1,7 @@
 uid="${UID}"
 apiKey="${API_KEY}"
+env_uid="${ENV_UID}
+
 
 newman run "https://api.getpostman.com/collections/$uid?apikey=$apiKey" \
-    --environment "https://api.getpostman.com/environments/$uid?apikey=$apiKey"
+    --environment "https://api.getpostman.com/environments/$env_uid?apikey=$apiKey"
